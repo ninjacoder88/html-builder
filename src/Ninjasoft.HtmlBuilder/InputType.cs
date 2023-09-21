dfa6a28bcdf7b3f8fd138c113fe3e2a4e9cad200
Tom Harris <tomharris88@outlook.com>
2023-09-21 17:21:53 -0500

    add input type

---
+++ src/Ninjasoft.HtmlBuilder/InputType.cs
@@ -0,0 +1,14 @@
+﻿namespace Ninjasoft.HtmlBuilder
+{
+    public sealed class InputType
+    {
+        private InputType(string type)
+        {
+            Type = type;
+        }
+
+        public string Type { get; }
+
+        public static InputType Text = new InputType("text");
+    }
+}
